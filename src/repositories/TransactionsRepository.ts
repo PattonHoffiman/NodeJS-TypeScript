ed6d8618c4409fa2e73eb8bd54abfb2720dc5dac
Patton Hoffiman <PattonHoffiman@gmail.com>
2020-11-03 15:11:08 -0300

    Can Create a New Transaction

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -13,16 +19,35 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
-  public all(): Transaction[] {
-    // TODO
-  }
+  /*  public all(): Transaction[] {
+
+  } */
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.sumTransactionValue('income');
+    const outcome = this.sumTransactionValue('outcome');
+    const total = income - outcome;
+
+    const balance: Balance = { income, outcome, total };
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
+  }
+
+  private sumTransactionValue(type: 'income' | 'outcome'): number {
+    let value = 0;
+
+    this.transactions
+      .filter(transaction => transaction.type === type)
+      .forEach(transaction => {
+        value += transaction.value;
+      });
+
+    return value;
   }
 }
 
